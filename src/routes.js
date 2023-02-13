ad7a8948c77be6c26f81bb32c94a82f4d435c66b
Tony Costa <tonyblack10@gmail.com>
2023-02-13 02:16:33 -0300

    add create task endpoint

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,36 @@ import { buildRoutePath } from './utils/build-route-path.js'
 const database = new Database()
 
 export const routes = [
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      if (!title) {
+        return res
+          .writeHead(400)
+          .end(JSON.stringify({ message: 'title is required' }))
+      }
+
+      if (!description) {
+        return res
+          .writeHead(400)
+          .end(JSON.stringify({ message: 'description is required' }))
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        created_at: new Date(),
+        updated_at: new Date(),
+        completed_at: null,
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    }
+  },
 ]
