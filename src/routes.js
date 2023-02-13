9211967a358b4c0ff9d47c4ccbc2845a2308a474
Tony Costa <tonyblack10@gmail.com>
2023-02-13 01:58:10 -0300

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+export const routes = [
+]
